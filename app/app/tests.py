6fae2daff0b2453a028662bf4e951d8f2c1b5fed
mgrunepy <mgrunepy@gmail.com>
2022-10-16 11:12:07 +0100

    Updating to include database and database connectors

---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """ Test the calc module """
+
+    def test_add_numbers(self):
+        """Test adding numbers"""
+        res = calc.add(10,-1)
+        self.assertEqual(res,9)
