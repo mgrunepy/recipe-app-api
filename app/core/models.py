cede7f3a2e6061de86d3afe5c9f9b765463b8d13
mgrunepy <mgrunepy@gmail.com>
2022-10-17 09:40:28 +0100

    User custom module updated

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,41 @@
-# Added # noqa to this import to get flake8 to ignore errors here
-from django.db import models  # noqa
+"""
+Database models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+
+    def create_user(self, email, password=None, **extra_field):
+        """Create, sanve and return new user."""
+
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+
+    # User values
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+
+    # Only staff can logon to admin pages
+    is_staff = models.BooleanField(default=False)
+
+    # Assign the user manager class to the User class
+    objects = UserManager()
+
+    # Set the field we want to use for authentication
+    USERNAME_FIELD = "email"
+
